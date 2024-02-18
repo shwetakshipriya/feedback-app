f9724387dceaced1300c40097a4337db8648c935
Shwetakshi Priya <shwetakshi1175.be21@chitkarauniversity.edu.in>
2024-02-18 14:30:29 +0530

    first commit

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import {useContext} from "react";
+import FeedbackContext from "../context/FeedbackContext";
+
+export default function FeedbackStats() {
+  // calculate rating average
+  const {feedback} = useContext(FeedbackContext);
+
+  let average = feedback.reduce((acc, cur) => {
+    return acc + cur.rating
+  }, 0) / feedback.length
+
+  average = average.toFixed(1).replace(/[.,]0$/, '')
+ 
+
+  return (
+    <div className="feedback-stats">
+      <h4>
+        {feedback.length + " "}
+        Reviews
+      </h4>
+      <h4>Average Rating: {isNaN(average) ? 0: average}</h4>
+    </div>
+  );
+}
+
+
