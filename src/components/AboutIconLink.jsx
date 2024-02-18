ab9ff0a4a92ee40b79b8fc7733a78a74151b75f4
Shwetakshi Priya <shwetakshi1175.be21@chitkarauniversity.edu.in>
2024-02-18 19:49:47 +0530

    about-icon-fixed

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 
 function AboutIconLink() {
   return (
-    <div className='about'>
+    <div className="about-link">
         <Link to='/about'>
         <FaQuestion size={30} color='white'/>
         </Link>  
